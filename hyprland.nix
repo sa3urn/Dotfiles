a51a1a506c45182ac1e3aa7200d2f2b4d826c678
q3e4ir <durychyaroslav@gmail.com>
2024-10-18 16:55:48 +0300

    commit

--- hyprland.nix
+++ hyprland.nix
@@ -8,127 +8,114 @@
       preload = "/home/q3e4ir/wallpaper.png";
       wallpaper = "eDP-1, /home/q3e4ir/wallpaper.png";
     };
+    ### HYPRLAND ###
     wayland.windowManager.hyprland.enable = true;
-    wayland.windowManager.hyprland.settings."###" = ''
-    ###
-    monitor=,preferred,auto,1
-    env = XCURSOR_SIZE,24
-    input {
-      kb_layout = us,ru
-      kb_variant =
-      kb_model =
-      kb_options = grp:win_space_toggle
-      kb_rules = 
+    wayland.windowManager.hyprland.settings = {
+      monitor = ",preferred,auto,1";
+      env = "XCURSOR_SIZE,24";
+      input = {
+        kb_layout = "us,ru";
+        kb_options = "grp:win_space_toggle";
+        follow_mouse = 1;
+        touchpad = {
+            natural_scroll = "yes";
+        };
+        sensitivity = -0.1;
+      };
+      general = {
+        gaps_in = 5;
+        gaps_out = 5;
+        border_size = 2;
+        "col.active_border" = "rgb(b4befe)";
+        "col.inactive_border" = "rgb(1e1e2e)";
+        layout = "dwindle";
+        allow_tearing = false;
+      };
 
-      follow_mouse = 1
+      decoration = {
+        rounding = 5;
+        blur = {
+          enabled = true;
+          size = 3;
+          passes = 1;
+        };
 
-      touchpad {
-          natural_scroll = yes
-      }
+        drop_shadow = "yes";
+        shadow_range = 4;
+        shadow_render_power = 3;
+        "col.shadow" = "rgba(1a1a1aee)";
+        active_opacity = 0.97;
+        inactive_opacity = 0.95;
+      };
 
-      sensitivity = -0.1
-    }
+      animations = {
+        enabled = "yes";
 
-    general {
-      gaps_in = 5
-      gaps_out = 5
-      border_size = 2
-      col.active_border = rgb(b4befe)
-      col.inactive_border = rgb(1e1e2e)
-      layout = dwindle
-      allow_tearing = false
-    }
+        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
 
-    decoration {
-      rounding = 5
+        animation = [
+          "windows, 1, 7, myBezier"
+          "windowsOut, 1, 7, default, popin 80%"
+          "border, 1, 10, default"
+          "borderangle, 1, 8, default"
+          "fade, 1, 7, default"
+          "workspaces, 1, 6, default"
+        ];
+      };
 
-      blur {
-        enabled = true
-        size = 3
-        passes = 1
-      }
+      dwindle = {
+        pseudotile = "yes";
+        preserve_split = "yes";
+      };
 
-      drop_shadow = yes
-      shadow_range = 4
-      shadow_render_power = 3
-      col.shadow = rgba(1a1a1aee)
-      active_opacity = 0.97
-      inactive_opacity = 0.95
-    }
+      gestures = {
+        workspace_swipe = "off";
+      };
 
-    animations {
-      enabled = yes
+      misc = {
+        force_default_wallpaper = 0 ;
+        disable_splash_rendering = true;
+        disable_hyprland_logo = true;
+      };
 
-      bezier = myBezier, 0.05, 0.9, 0.1, 1.05
+      "$mainMod" = "SUPER";
+      "$editor" = "codium";
+      exec-once = ["waybar" "hyprpaper"];
 
-      animation = windows, 1, 7, myBezier
-      animation = windowsOut, 1, 7, default, popin 80%
-      animation = border, 1, 10, default
-      animation = borderangle, 1, 8, default
-      animation = fade, 1, 7, default
-      animation = workspaces, 1, 6, default
-    }
-
-    dwindle {
-      pseudotile = yes
-      preserve_split = yes 
-    }
-
-
-    gestures {
-      workspace_swipe = off
-    }
-
-    misc {
-      force_default_wallpaper = 0 
-      disable_splash_rendering = true
-      disable_hyprland_logo = true
-    }
-
-    $mainMod = SUPER
-    $editor = codium
-
-    exec-once = waybar
-    exec-once = hyprpaper
-
-    windowrulev2 = stayfocused,       class:(Lxpolkit)
-
-    bind = $mainMod, RETURN,                  exec, kitty --hold
-
-    bind = $mainMod, A,                       exec, wofi --show drun
-
-    bind = $mainMod, W,                       killactive, 
-    bind = $mainMod, M,                       exit, 
-    bind = $mainMod, F,                       togglefloating, 
-    bind = $mainMod, J,                       togglesplit, 
-
-    bind = $mainMod, 1,                       workspace, 1
-    bind = $mainMod, 2,                       workspace, 2
-    bind = $mainMod, 3,                       workspace, 3
-    bind = $mainMod, 4,                       workspace, 4
-    bind = $mainMod, 5,                       workspace, 5
-    bind = $mainMod, 6,                       workspace, 6
-    bind = $mainMod, 7,                       workspace, 7
-    bind = $mainMod, 8,                       workspace, 8
-    bind = $mainMod, 9,                       workspace, 9
-    bind = $mainMod, 0,                       workspace, 10
-
-    bind = $mainMod SHIFT, 1,                 movetoworkspace, 1
-    bind = $mainMod SHIFT, 2,                 movetoworkspace, 2
-    bind = $mainMod SHIFT, 3,                 movetoworkspace, 3
-    bind = $mainMod SHIFT, 4,                 movetoworkspace, 4
-    bind = $mainMod SHIFT, 5,                 movetoworkspace, 5
-    bind = $mainMod SHIFT, 6,                 movetoworkspace, 6
-    bind = $mainMod SHIFT, 7,                 movetoworkspace, 7
-    bind = $mainMod SHIFT, 8,                 movetoworkspace, 8
-    bind = $mainMod SHIFT, 9,                 movetoworkspace, 9
-    bind = $mainMod SHIFT, 0,                 movetoworkspace, 10
-
-    bind = $mainMod, mouse_down, workspace,   e+1
-    bind = $mainMod, mouse_up, workspace,     e-1
-
-    bindm = $mainMod, mouse:272,              movewindow
-    bindm = $mainMod, mouse:273,              resizewindow
-    '';
+      bind = [
+      "$mainMod, RETURN, exec, kitty --hold"
+      "$mainMod, A, exec, wofi --show drun"
+      "$mainMod, W, killactive"
+      "$mainMod, M, exit"
+      "$mainMod, F, togglefloating"
+      "$mainMod, J, togglesplit," 
+      "$mainMod, 1, workspace, 1"
+      "$mainMod, 2, workspace, 2"
+      "$mainMod, 3, workspace, 3"
+      "$mainMod, 4, workspace, 4"
+      "$mainMod, 5, workspace, 5"
+      "$mainMod, 6, workspace, 6"
+      "$mainMod, 7, workspace, 7"
+      "$mainMod, 8, workspace, 8"
+      "$mainMod, 9, workspace, 9"
+      "$mainMod, 0, workspace, 10"
+      "$mainMod SHIFT, 1, movetoworkspace, 1"
+      "$mainMod SHIFT, 2, movetoworkspace, 2"
+      "$mainMod SHIFT, 3, movetoworkspace, 3"
+      "$mainMod SHIFT, 4, movetoworkspace, 4"
+      "$mainMod SHIFT, 5, movetoworkspace, 5"
+      "$mainMod SHIFT, 6, movetoworkspace, 6"
+      "$mainMod SHIFT, 7, movetoworkspace, 7"
+      "$mainMod SHIFT, 8, movetoworkspace, 8"
+      "$mainMod SHIFT, 9, movetoworkspace, 9"
+      "$mainMod SHIFT, 0, movetoworkspace, 10"
+      "$mainMod, mouse_down, workspace, e+1"
+      "$mainMod, mouse_up, workspace, e-1"
+      ];
+      bindm = [
+        "$mainMod, mouse:272, movewindow"
+        "$mainMod, mouse:273, resizewindow"
+      ];
+    };
   };
 }
