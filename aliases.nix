6200995a0d7b8d0b672f8ef2a575824b8cefafc9
sa3urn <165802189+sa3urn@users.noreply.github.com>
2024-11-27 16:25:51 +0200

    Update aliases.nix

--- aliases.nix
+++ aliases.nix
@@ -4,13 +4,13 @@
   programs.bash.shellAliases.sshlog =
 
   ''
-  ssh-keygen -t rsa -b 4096 -P "" -f /home/q3e4ir/.ssh/id_rsa
+  ssh-keygen -t rsa -b 4096 -P "" -f /home/sa3urn/.ssh/id_rsa
   cat ~/.ssh/id_rsa.pub; echo 'https://github.com/settings/ssh/new'
   read -p 'apply ssh-key' apply
   cd ~/dotfiles
   git init
   git remote rm origin
-  git remote add origin git@github.com:q3e4ir/dotfiles.git
+  git remote add origin git@github.com:sa3urn/dotfiles.git
   '';
 
   programs.bash.shellAliases.dotcommit  =
@@ -27,7 +27,7 @@
   ''
   sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz home-manager
   sudo nix-channel --update
-  sudo cp -r /home/q3e4ir/dotfiles/* /etc/nixos/
+  sudo cp -r /home/sa3urn/dotfiles/* /etc/nixos/
   sudo nixos-rebuild switch --upgrade
   sudo nix-collect-garbage --delete-older-than 1d
   '';
